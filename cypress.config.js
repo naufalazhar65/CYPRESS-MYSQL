7efb2ef74008c203ad4b6d2d106ab301dc3492f7
Naufal Azhar <naufalazhar65@gmail.com>
2023-12-11 21:17:58 +0700

    commit

--- cypress.config.js
+++ cypress.config.js
@@ -2,35 +2,55 @@ const { defineConfig } = require("cypress");
 const mysql = require("mysql2");
 
 module.exports = defineConfig({
+  reporter: "mochawesome",
+  reporterOptions: {
+    charts: true,
+    overwrite: false,
+    html: false,
+    json: true,
+    reportDir: "cypress/reports/mocha"
+  },
   e2e: {
+    // Menggunakan setupNodeEvents untuk menangani event dan task pada Cypress
     setupNodeEvents(on, config) {
+   // Menambahkan task khusus "queryDb" untuk menjalankan query pada database
       on("task", {
         queryDb: (query) => {
+          // Memanggil fungsi queryTestDb dengan query dan konfigurasi dari Cypress
           return queryTestDb(query, config);
         },
       });
     },
+    // Mengatur konfigurasi environment, khususnya informasi database
     env: {
       db: {
-        server: "127.0.0.1",
-        user: "root",
-        password: "",
-        database: "belajar_mysql",
-        port: "3306",
+        server: "127.0.0.1", // Alamat server database
+        user: "root", // Nama pengguna database
+        password: "", // Kata sandi database
+        database: "belajar_mysql", // Nama database
+        port: "3306", // Port database
       },
     },
   },
 });
 
+// Fungsi untuk menjalankan query pada database
 function queryTestDb(query, config) {
+  // Membuat koneksi MySQL menggunakan konfigurasi dari environment Cypress
   const connection = mysql.createConnection(config.env.db);
+  // Membuka koneksi ke database
   connection.connect();
 
+  // Mengembalikan promise untuk menangani hasil query
   return new Promise((resolve, reject) => {
+    // Menjalankan query pada database
     connection.query(query, (error, result) => {
+      // Menangani kesalahan (error) jika ada
       if (error) reject(error);
       else {
+        // Menutup koneksi ke database
         connection.end();
+        // Mengembalikan hasil query menggunakan resolve
         return resolve(result);
       }
     });
--- cypress/e2e/myDBTest.cy.js
+++ cypress/e2e/myDBTest.cy.js
@@ -4,9 +4,9 @@ describe("data base", () => {
   it("query admin", () => {
     cy.task("queryDb", "select * from belajar_mysql.admin;").then((res) => {
       expect(res).to.have.length.above(0);
-      expect(res[0]).to.have.property("id");
-      expect(res[1]).to.have.property("first_name");
-      expect(res[2]).to.have.property("last_name");
+      expect(res[1]).to.have.property("id", 5);
+      expect(res[1]).to.have.property("first_name", "Naufal");
+      expect(res[1]).to.have.property("last_name", "Azhar");
       var rec = res;
       const result = Object.values(rec[0]);
       cy.log(result[0]);
@@ -17,13 +17,13 @@ describe("data base", () => {
   it("query products", () => {
     cy.task("queryDb", "select * from belajar_mysql.products;").then((res) => {
       expect(res).to.have.length.above(0);
-      expect(res[0]).to.have.property("id");
-      expect(res[1]).to.have.property("name");
-      expect(res[2]).to.have.property("category");
-      expect(res[3]).to.have.property("description");
-      expect(res[4]).to.have.property("price");
-      expect(res[5]).to.have.property("quantity");
-      expect(res[6]).to.have.property("created_at");
+      expect(res[1]).to.have.property("id", "P0002");
+      expect(res[1]).to.have.property("name", "Mie Ayam Bakso");
+      expect(res[1]).to.have.property("category", "Makanan");
+      expect(res[1]).to.have.property("description", "Mie Ayam Original + baksor");
+      expect(res[1]).to.have.property("price", 20000);
+      expect(res[1]).to.have.property("quantity", 100);
+      expect(res[1]).to.have.property("created_at");
       var rec = res;
       const result = Object.values(rec[0]);
       cy.log(result[0]);
@@ -35,6 +35,35 @@ describe("data base", () => {
     cy.task("queryDb", "select * from belajar_mysql.barang;").then((res) => {
       expect(res).to.have.length.above(0);
       expect(res[0]).to.have.property("id");
+      var rec = res;
+      const result = Object.values(rec[0]);
+      cy.log(result[0]);
+      cy.log(result[1]);
+    });
+  });
+
+  it("query orders", () => {
+    cy.task("queryDb", "select * from belajar_mysql.orders;").then((res) => {
+      expect(res).to.have.length.above(0);
+      expect(res[0]).to.have.property("id");
+      expect(res[1]).to.have.property("total");
+      expect(res[1]).to.have.property("order_date");
+      // expect(res[3]).to.have.property("quantity");
+      var rec = res;
+      const result = Object.values(rec[0]);
+      cy.log(result[0]);
+      cy.log(result[1]);
+    });
+  });
+
+  it("query orders_detail", () => {
+    cy.task("queryDb", "select * from belajar_mysql.orders_detail;").then((res) => {
+      expect(res).to.have.length.above(0);
+      expect(res[0]).to.have.property("id_product");
+      expect(res[1]).to.have.property("id_order");
+      expect(res[1]).to.have.property("price");
+      expect(res[1]).to.have.property("quantity");
+
       var rec = res;
       const result = Object.values(rec[0]);
       cy.log(result[0]);
